8e19405651733e57c6e0dc464bda3fcb3a1cc4bd
Dennis <denjand91@gmail.com>
2022-02-03 04:35:05 -0500

    finished

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from http.client import responses
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template,redirect,flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -16,9 +16,36 @@ def index():
     instructions = satisfaction_survey.instructions
     return render_template("start.html",title=title, instructions=instructions)
 
-@app.route('/question/<question_num>')
+@app.route('/question/<int:question_num>')
 def quest(question_num):
-    "Show page with appropriately numbered question"
-    question = satisfaction_survey.questions[int(question_num)]
-    choices = question.choices
-    return render_template("question.html", question=question ,num=len(responses)+1, choices= choices)
+    """Show page with appropriately numbered question"""
+    #if len(responses) == 0:
+    #   return redirect("/")
+    
+    if len(responses) == len(satisfaction_survey.questions):
+        return redirect("/")
+
+    if len(responses) != (question_num):
+        flash("You have skipped ahead, returning you to your question")
+        return redirect(f"/question/{len(responses)}")
+    question = satisfaction_survey.questions[question_num]
+    choices =question.choices    
+    return render_template("question.html", question=question, choices=choices)
+
+@app.route('/answer', methods=["POST"])
+def get_answer():
+    """Get response and move to next question"""
+    
+    survey_answer=request.form["s_question"]
+    responses.append(survey_answer)
+    
+    if len(responses) == len(satisfaction_survey.questions):
+        return redirect("/thanks")
+    else: 
+        return redirect(f"/question/{len(responses)}")
+
+@app.route("/thanks")
+def thanks():
+    """THank you page for completing the survey"""
+
+    return render_template("/thanks.html")
